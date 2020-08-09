c3aff5e3498a6b54fd2348a286e45a7f2e48b5dc
katsulau <efthnyb.1994@gmail.com>
2020-08-09 11:22:32 +0900

    add sample code

--- src/model/sample/TokenCheck.kt
+++ src/model/sample/TokenCheck.kt
@@ -3,10 +3,10 @@ package model.sample
 class TokenCheck {
 
     fun <T> authorized(token: String, action: (userId: String) -> T): T {
-        val userId = when {
-            token == "aaa" -> "userA"
-            token == "bbb" -> "userB"
-            else  -> "other user"
+        val userId = when (token) {
+            "aaa" -> "userA"
+            "bbb" -> "userB"
+            else -> "other user"
         }
 
         return action.invoke(userId)
